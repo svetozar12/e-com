cee14f99df24e4925b7fe0a5ab4fe1ee5a71bb5c
svetozar12 <zaauna@abv.bg>
2023-03-28 12:49:08 -0700

    fix build

--- libs/grpc-clients/src/lib/Book.client.ts
+++ libs/grpc-clients/src/lib/Book.client.ts
@@ -5,12 +5,12 @@ import { ProtoGrpcType } from '@ms-learning/book-service-proto';
 const packageDefinition = protoLoader.loadSync(
   resolve('libs/protos/book-service-proto/src/lib/book.v1.proto')
 );
-const bookProto: ProtoGrpcType = grpc.loadPackageDefinition(packageDefinition);
-const client = new bookProto.book.v1.BookService(
-  'localhost:1000',
-  grpc.credentials.createInsecure()
-);
-client.FindOne({ id: 1 }, function (err, response) {
-  console.log('Data:', response); // API response
-  console.log(err);
-});
+// const bookProto: ProtoGrpcType = grpc.loadPackageDefinition(packageDefinition);
+// const client = new bookProto.book.v1.BookService(
+//   'localhost:1000',
+//   grpc.credentials.createInsecure()
+// );
+// client.FindOne({ id: 1 }, function (err, response) {
+//   console.log('Data:', response); // API response
+//   console.log(err);
+// });
