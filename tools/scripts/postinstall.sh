7b7370a6cf2096f2b464585691e29e89def94769
svetozar12 <s.gospodinov02@gmail.com>
2023-08-01 21:18:40 +0300

    added gow for hot reloading

--- tools/scripts/postinstall.sh
+++ tools/scripts/postinstall.sh
@@ -3,3 +3,5 @@ go mod download \
   && go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@v1.2 \
   && go install github.com/grpc-ecosystem/grpc-gateway/v2/protoc-gen-grpc-gateway@latest \
   && go install github.com/google/gnostic/cmd/protoc-gen-openapi@latest
+
+go install github.com/mitranim/gow@latest
