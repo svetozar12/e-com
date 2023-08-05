dbcc409609b27e5960051cd8a349330e7a020ffa
svetozar12 <s.gospodinov02@gmail.com>
2023-08-05 11:29:53 +0300

    hotfix

--- apps/services/cart/internal/pkg/env/env.go
+++ apps/services/cart/internal/pkg/env/env.go
@@ -10,7 +10,6 @@ import (
 type Config struct {
 	Port                        string `env:"CART_SERVICE_PORT" envDefault:"9005"`
 	POSTGRES_CONNECTION_STRING  string `env:"CART_SERVICE_POSTGRES_CONNECTION_STRING" envDefault:"postgres://postgres:postgrespw@localhost:5432"`
-	InventoryServiceAdress      string `env:"CART_SERVICE_INVENTORY_SERVICE_ADDRESS" envDefault:"0.0.0.0:9003"`
 	ProductCatalogServiceAdress string `env:"CART_SERVICE_PRODUCT_CATALOG_SERVICE_ADDRESS" envDefault:"0.0.0.0:9001"`
 }
 
