4caffc1ec3024a164543fdbd47f3aa08b3070c4f
svetozar12 <s.gospodinov02@gmail.com>
2023-08-20 20:37:58 +0300

    .

--- apps/services/aggregator/main.go
+++ apps/services/aggregator/main.go
@@ -1,7 +1,19 @@
 package main
 
-import "svetozar12/e-com/v2/apps/services/aggregator/internal/app/bootstrap"
+import (
+	"log"
+	"net/http"
+	"svetozar12/e-com/v2/apps/services/aggregator/internal/app/bootstrap"
+	"svetozar12/e-com/v2/apps/services/aggregator/internal/pkg/env"
+)
 
 func main() {
 	bootstrap.Bootstrap()
+	fs := http.FileServer(http.Dir("../../../swagger.html"))
+	http.Handle("/swagger", fs)
+	port := ":" + env.Envs.Port
+	err := http.ListenAndServe(port, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
---
+++ tools/scripts/mergeSwagger.py
@@ -0,0 +1,35 @@
+import json
+
+OUTPUT_FILE = "merged-swagger.json"
+
+# Function that generates the Swagger JSON file path
+def get_oapi_path(proto_name):
+    return f"api/v1/{proto_name}/dist/openapi/{proto_name}.swagger.json"
+
+# Initialize the merged data
+merged_data = {
+    "swagger": "2.0",
+    "info": {
+        "title": "Merged API Documentation",
+        "version": "1.0"
+    },
+    "paths": {},
+    "definitions": {}
+}
+
+# List of proto names
+proto_names = ["cart", "inventory"]
+
+# Merge JSON files
+for proto_name in proto_names:
+    oapi_path = get_oapi_path(proto_name)
+    with open(oapi_path, "r") as file:
+        data = json.load(file)
+        merged_data["paths"].update(data.get("paths", {}))
+        merged_data["definitions"].update(data.get("definitions", {}))
+
+# Write merged data to output file
+with open(OUTPUT_FILE, "w") as output_file:
+    json.dump(merged_data, output_file, indent=2)
+
+print(f"Merged Swagger JSON file saved as {OUTPUT_FILE}")
