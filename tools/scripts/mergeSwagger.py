770de033dc76ba7bce13d17e541252e10d94a50b
svetozar12 <s.gospodinov02@gmail.com>
2023-08-21 20:47:28 +0300

    same

--- tools/scripts/mergeSwagger.py
+++ tools/scripts/mergeSwagger.py
@@ -1,76 +1,49 @@
-import json
+import yaml
 
-OUTPUT_FILE = "./apps/services/aggregator/third_party/swagger.json"
+OUTPUT_FILE = "./apps/services/aggregator/third_party/swagger.yaml"
 
-# Function that generates the Swagger JSON file path
+# Function that generates the OpenAPI YAML file path
 def get_oapi_path(proto_name):
-    return f"api/v1/{proto_name}/dist/openapi/{proto_name}.swagger.json"
+    return f"api/v1/{proto_name}/dist/OpenAPI/openapi.yaml"
 
 # Initialize the merged data
 merged_data = {
-    "swagger": "2.0",
+    "openapi": "3.0.0",
     "info": {
         "title": "Merged API Documentation",
         "version": "1.0"
     },
-    "securityDefinitions": {
-        "bearerAuth": {
-            "type": "apiKey",
-            "name": "Authorization",
-            "in": "header",
-            "description": "Bearer token authorization"
-        }
+    "components": {
+        "securitySchemes": {
+            "bearerAuth": {
+                "type": "apiKey",
+                "name": "Authorization",
+                "in": "header"
+            }
+        },
+        "schemas": {}
     },
     "security": [
         {
             "bearerAuth": []
         }
     ],
-    "paths": {},
-    "definitions": {}
+    "paths": {}
 }
 
 # List of proto names
-proto_names = ["cart", "file-upload", "inventory", "notification", "order", "payment", "product-catalog", "review", "user"]
+proto_names = ["product-catalog","user"]
 
-# Merge JSON files
+# Merge YAML files
 for proto_name in proto_names:
     oapi_path = get_oapi_path(proto_name)
     with open(oapi_path, "r") as file:
-        data = json.load(file)
+        data = yaml.safe_load(file)
         merged_data["paths"].update(data.get("paths", {}))
-        merged_data["definitions"].update(data.get("definitions", {}))
+        merged_data["components"]["schemas"].update(data.get("components", {}).get("schemas", {}))
 
-# Add example multipart/form-data endpoint
-example_form_data_path = "/example/form-data"
-merged_data["paths"][example_form_data_path] = {
-    "post": {
-        "summary": "Example Form Data Endpoint",
-        "consumes": ["multipart/form-data"],
-        "produces": ["application/json"],
-        "parameters": [
-            {
-                "name": "file",
-                "in": "formData",
-                "type": "file",
-                "required": True
-            },
-            {
-                "name": "additional_field",
-                "in": "formData",
-                "type": "string"
-            }
-        ],
-        "responses": {
-            "200": {
-                "description": "Success response"
-            }
-        }
-    }
-}
-
-# Write merged data to the output file
+# Convert merged data to YAML and write to the output file
 with open(OUTPUT_FILE, "w") as output_file:
-    json.dump(merged_data, output_file, indent=2)
+    yaml.dump(merged_data, output_file)
 
-print(f"Merged Swagger JSON file saved as {OUTPUT_FILE}")
+print(f"Merged OpenAPI YAML file saved as {OUTPUT_FILE}")
