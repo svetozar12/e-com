ecd6c7050104affa2818b66292f4589fffdbc36c
svetozar12 <s.gospodinov02@gmail.com>
2023-08-24 21:47:39 +0300

    ddaw

--- tools/scripts/buildSwagger.sh
+++ tools/scripts/buildSwagger.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
+REPO_URL="https://github.com/swagger-api/swagger-ui.git"
+CACHE_DIR="$HOME/.cached_repos/swagger-ui"
+
 rm -r apps/services/aggregator/third_party/swagger-build
 mkdir -p apps/services/aggregator/third_party/swagger-build
 
 python3 tools/scripts/mergeSwagger.py
 cd apps/services/aggregator/third_party/swagger-build
-git clone https://github.com/swagger-api/swagger-ui.git
+if [ ! -d "$CACHE_DIR" ]; then
+    echo "Cloning repository for the first time..."
+    git clone "$REPO_URL" "$CACHE_DIR"
+else
+    echo "Repository already cached."
+fi
+
+cp -r $CACHE_DIR .
