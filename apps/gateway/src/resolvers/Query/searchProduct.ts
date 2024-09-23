560d9314e2e9bc4ce429809ce084b4b300cabdd9
svetozar12 <s.gospodinov02@gmail.com>
2024-09-23 18:52:29 +0300

    working on issues with deloying

--- apps/gateway/src/resolvers/Query/searchProduct.ts
+++ apps/gateway/src/resolvers/Query/searchProduct.ts
@@ -1,7 +1,5 @@
 import { formatError } from '../../utils/error';
 import { sdk } from '../../utils/sdk';
 import type { QueryResolvers } from './../../codegen/types.generated';
-export const searchProduct: NonNullable<
-  QueryResolvers['searchProduct']
-> = async (_parent, { searchText }) =>
+export const searchProduct: NonNullable<QueryResolvers['searchProduct']> = async (_parent, { searchText }) =>
   sdk.search().searchProducts(searchText).catch(formatError);
